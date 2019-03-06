cfa184ec54746cbaedd8d902b1ea796db297c604
Michael Huyler <michaelhuyler@gmail.com>
2019-03-05 19:57:08 -0600

    Parsing ingredients/tools/verbs per step

--- recipes.py
+++ recipes.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+import random
 import ssl
 import sys
 from urllib.request import urlopen
@@ -12,19 +13,60 @@ import spacy
 ssl._create_default_https_context = ssl._create_unverified_context
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
-urls = ['https://www.allrecipes.com/recipe/18511/hash-brown-casserole-ii/',
-        'https://www.allrecipes.com/recipe/212721/indian-chicken-curry-murgh-kari/',
-        'https://www.allrecipes.com/recipe/91192/french-onion-soup-gratinee/',
-        'https://www.allrecipes.com/recipe/60598/vegetarian-korma/'
-        ]
-url = urls[2]
-page = urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
-ingredients = []
-cooking_verbs = ['bake', 'barbeque', 'baste', 'batter', 'beat', 'blanch', 'blend', 'boil', 'broil', 'carmelize', 'chop', 'clarify', 'cream', 'cure', 'deglaze', 'degrease', 'dice', 'dissolve' 'dredge', 'drizzle', 'dust', 'fillet', 'flake', 'flambe', 'fold', 'fricasse', 'fry', 'garnish', 'glaze', 'grate', 'grind', 'julienne', 'knead', 'marinate', 'meuniere', 'mince', 'mix', 'pan-broil', 'pan-fry', 'parboil', 'pare', 'peel', 'pickle', 'pit', 'plump', 'poach', 'puree', 'reduce', 'refresh', 'roast', 'saute', 'scald', 'scallop', 'score', 'sear', 'season', 'shred', 'sift', 'simmer', 'skim', 'steam', 'steep', 'sterilize', 'stew', 'stir', 'toss', 'truss', 'whip', 'preheat']
-to_be_verbs = ['be', 'is', 'are', 'was']
-measurements_list = ['bunch', 'clove', 'pinch', 'slice', 'sprig', 'cup', 'tablespoon', 'teaspoon', 'ounce', 'can', 'pint', 'quart', 'gallon', 'tsp', 'tbsp', 'tblsp', 'tbs', 'lb', 'pound', 'oz', 'c', 'pt', 'qt', 'gal']
 
+base_url = 'https://www.allrecipes.com/recipe/'
+urls = [
+    '-1',
+    base_url + '18511',
+    base_url + '212721',
+    base_url + '91192',
+    base_url + '60598',
+    base_url + '103895'
+    ]
+# AllRecipes URL to parse
+url = None
+# The HTML parsed by BeautifulSoup
+soup = None
+
+ingredients = {}
+cooking_verbs = [
+    'bake', 'barbeque', 'baste', 'batter', 'beat', 'blanch', 'blend', 'boil',
+    'broil', 'carmelize', 'chop', 'clarify', 'cream', 'cure', 'deglaze',
+    'degrease', 'dice', 'dissolve' 'dredge', 'drizzle', 'dust', 'fillet',
+    'flake', 'flambe', 'fold', 'fricasse', 'fry', 'garnish', 'glaze', 'grate',
+    'grind', 'julienne', 'knead', 'marinate', 'meuniere', 'mince', 'mix',
+    'pan-broil', 'pan-fry', 'parboil', 'pare', 'peel', 'pickle', 'pit', 'plump',
+    'poach', 'puree', 'reduce', 'refresh', 'roast', 'saute', 'scald', 'scallop',
+    'score', 'sear', 'season', 'shred', 'sift', 'simmer', 'skim', 'steam',
+    'steep', 'sterilize', 'stew', 'stir', 'toss', 'truss', 'whip', 'preheat'
+    ]
+to_be_verbs = [
+    'be', 'is', 'are', 'was'
+    ]
+measurements_list = [
+    # Unique Collections
+    'bunch', 'can', 'clove', 'pinch', 'slice', 'sprig',
+    # Volumes
+    'tsp', 'teaspoon', 'tbsp', 'tblsp', 'tbs', 'tablespoon', 'c', 'cup', 'pt', 'pint', 'qt', 'quart', 'gal', 'gallon',
+    'ml', 'mL', 'milliliter', 'millilitre', 'l', 'L', 'liter', 'litre',
+    # Weights
+    'oz', 'ounce', 'lb', 'pound',
+    'mg', 'milligram', 'milligramme', 'g', 'gram', 'gramme', 'kg', 'kilogram', 'kilogramme',
+    # Lengths
+    'in', 'inch',
+    'cm', 'centimeter', 'centimetre'
+    ]
+tools = [
+    'spoon', 'bowl', 'skillet', 'oven', 'knife', 'whisk', 'fork',
+    'cutting board', 'can opener', 'strainer', 'sieve', 'blender', 'pan', 'pot',
+    'saucepan', 'peeler', 'measuring cup', 'scoop', 'measuring spoon',
+    'colander', 'masher', 'salad spinner', 'grater', 'shears', 'rolling pin',
+    'juicer', 'garlic press', 'dish', 'plate', 'platter', 'foil', 'stockpot',
+    'crockpot', 'spatula', 'tongs', 'ladle', 'trivet', 'lid', 'splatter guard',
+    'paper towel', 'thermometer', 'scale', 'parchment paper', 'baking sheet',
+    'glass', 'cup', 'tray', 'press', 'microwave', 'stove', 'stovetop', 'kettle',
+    'toaster', 'chopper', 'double boiler', 'steamer'
+    ]
 
 class Step:
     def __init__(self, text):
@@ -34,14 +76,55 @@ class Step:
     def __parse__(self):
         nlp = spacy.load('en_core_web_sm')
         tokens = nlp(self.text)
+        self.ingredients = []
+        for i in range(len(tokens)):
+            if (not tokens[i].tag_.startswith('NN')) or tokens[i].text in measurements_list:
+                continue
+            for ingredient in ingredients.keys():
+                if tokens[i].text in ingredient:
+                    # print("Starting with: " + tokens[i].text)
+                    potential_ingredient = tokens[i].text
+                    j = i - 1
+                    while j > 0 and tokens[j].text in ingredient:
+                        potential_ingredient = tokens[j].text + " " + potential_ingredient
+                        j -= 1
+                    self.ingredients.append(potential_ingredient)
+        removals = []
+        for i in range(len(self.ingredients)):
+            for j in range(len(self.ingredients)):
+                if self.ingredients[i] in self.ingredients[j] and self.ingredients[i] != self.ingredients[j]:
+                    removals.append(i)
+        self.ingredients = [self.ingredients[i] for i in range(len(self.ingredients)) if i not in removals]
+        for i in range(len(self.ingredients)):
+            self.ingredients[i] = find_largest_intersection(self.ingredients[i])
+        self.ingredients = [i for i in self.ingredients if i]
         self.verbs = []
+        self.tools = []
         for token in tokens:
             # print(token.text + "\t" + token.tag_ + "\t" + token.dep_)
             if token.tag_ in ["VB", "VBP", "VBZ"] and token.text not in to_be_verbs:
                 self.verbs.append(token)
+            elif token.text in tools:  # token.tag_ in ["NN"] and
+                self.tools.append(token.text)
 
     def get_verbs(self):
         return [v.text for v in self.verbs]
+
+    def get_tools(self):
+        return self.tools
+
+
+def find_largest_intersection(ingredient):
+    ingredient_set = set(ingredient.split())
+    largest_intersection = 0
+    real_ingredient = None
+    for each in ingredients:
+        real_set = set(each.split())
+        intersect = len(ingredient_set.intersection(real_set))
+        if intersect > largest_intersection:
+            largest_intersection = intersect
+            real_ingredient = each
+    return real_ingredient
 
 
 def intersection(lst1, lst2):
@@ -49,6 +132,18 @@ def intersection(lst1, lst2):
     if len(lst3) > 0:
         print("Intersection: " + str(lst3))
     return lst3
+
+
+def get_recipe(in_url):
+    global url
+    global soup
+    if in_url == '-1':
+        url = base_url + str(random.randint(60_000, 250_000))
+        print(url)
+    else:
+        url = in_url
+    page = urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
 
 
 def get_ingredients():
@@ -65,33 +160,25 @@ def get_ingredients():
         product = 1
         posn = 0
         for i in range(len(tokens)):
-            # print(tokens[i].text + "\t" + tokens[i].tag_ + "\t" + tokens[i].dep_)
             # Quantities
-            if tokens[i].tag_ in ['CD', 'LS'] and tokens[i + 1].tag_ != '(':
-                sum += float(Fraction(tokens[i].text))
-                posn = i
-            elif tokens[i].tag_ in ['CD', 'LS'] and tokens[i + 1].tag_ == '(':
-                product = float(Fraction(tokens[i].text))
-                posn = i
+            if tokens[i].tag_ in ['CD', 'LS'] and tokens[i + 1].text != '(':
+                try:
+                    sum += float(Fraction(tokens[i].text))
+                    posn = i
+                except Exception as e:
+                    descriptor.append(i)
+            elif tokens[i].tag_ in ['CD', 'LS'] and tokens[i + 1].text == '(':
+                try:
+                    product = float(Fraction(tokens[i].text))
+                    posn = i
+                except Exception as e:
+                    descriptor.append(i)
             # Descriptors
             elif tokens[i].tag_.startswith('JJ') and tokens[i].dep_ not in ['nsubj', 'dobj', 'pobj']:
                 descriptor.append(i)
             # Prep steps
             elif tokens[i].tag_ in ['VBN', 'RB']:
                 preparation.append(i)
-        # for i in range(len(tags)):
-        #     if tags[i][1] == 'CD' and tags[i + 1][1] != '(':
-        #         sum += float(Fraction(tags[i][0]))
-        #         posn = i
-        #     elif tags[i][1] == 'CD' and tags[i + 1][1] == '(':
-        #         product = float(Fraction(tags[i][0]))
-        #         posn = i
-        #     elif tags[i][1].startswith('JJ'): #descriptor words
-        #         descriptor.append(tags[i][0])
-        #     elif tags[i][1].startswith('VB'): #prep verbs
-        #         prep.append(tags[i][0])
-        #     elif tags[i][1].startswith('NN'): #its a name
-        #         name.append(tags[i][0])
         product = sum * product
         potential_unit = tokens[posn + 1]
         if not potential_unit.tag_.startswith('JJ'):
@@ -115,7 +202,7 @@ def get_ingredients():
         name = name.strip()
         desc = desc.strip()
         prep = prep.strip()
-        ingredients.append((product if product > 0 else None, unit, desc, prep, name))
+        ingredients[text] = (product if product > 0 else None, unit, desc, prep, name)
 
 
 def is_unit(text):
@@ -134,16 +221,21 @@ def get_instructions():
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        get_recipe(sys.argv[1])
+    else:
+        get_recipe(urls[0])
     get_ingredients()
-    for ingredient in ingredients:
-        line = ''
-        for i in ingredient:
-            if i is None:
-                continue
-            line += str(i).strip() + " "
-        print(line)
+    for external_rep, internal_rep in ingredients.items():
+        print(external_rep)
+        line = ""
+        for i in internal_rep:
+            line += ((str(i).strip() + " | ") if i else "_ | ")
+        print(">> " + line + "\n")
     steps = get_instructions()
     for x in range(len(steps)):
-        print("Step " + str(x + 1) + ": " + str(steps[x].text))
+        print("Step " + str(x + 1) + ": " + str(steps[x].text[4:5].upper()) + str(steps[x].text[5:]))
         print(steps[x].get_verbs())
+        print(steps[x].get_tools())
+        print(steps[x].ingredients)
         print("")
