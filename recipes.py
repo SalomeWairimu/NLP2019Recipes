df0fd8b6771749344ccd1a6a74d336335391e9c5
Michael Huyler <michaelhuyler@gmail.com>
2019-03-06 22:24:33 -0600

    Began vegetarian replacements

--- recipes.py
+++ recipes.py
@@ -33,15 +33,23 @@ soup = None
 ingredients = {}
 cooking_verbs = [
     'bake', 'barbeque', 'baste', 'batter', 'beat', 'blanch', 'blend', 'boil',
-    'broil', 'carmelize', 'chop', 'clarify', 'cream', 'cure', 'deglaze',
+    'broil', 'carmelize', 'chop', 'cook', 'clarify', 'cream', 'cure', 'deglaze',
     'degrease', 'dice', 'dissolve' 'dredge', 'drizzle', 'dust', 'fillet',
     'flake', 'flambe', 'fold', 'fricasse', 'fry', 'garnish', 'glaze', 'grate',
     'grind', 'julienne', 'knead', 'marinate', 'meuniere', 'mince', 'mix',
     'pan-broil', 'pan-fry', 'parboil', 'pare', 'peel', 'pickle', 'pit', 'plump',
-    'poach', 'puree', 'reduce', 'refresh', 'roast', 'saute', 'scald', 'scallop',
-    'score', 'sear', 'season', 'shred', 'sift', 'simmer', 'skim', 'steam',
-    'steep', 'sterilize', 'stew', 'stir', 'toss', 'truss', 'whip', 'preheat'
+    'poach', 'puree', 'refresh', 'roast', 'saute', 'scald', 'scallop',
+    'score', 'sear', 'season', 'shred', 'sift', 'simmer', 'skim', 'steam', 'serve', 'sprinkle',
+    'steep', 'sterilize', 'stew', 'stir', 'toss', 'truss', 'whip', 'preheat', 'caramelize', 'melt'
     ]
+primary_verbs = ['bake', 'boil', 'fry', 'simmer', 'saute', 'roast', 'steam', 'stew']
+meat_products = [
+    'beef', 'chicken', 'pork', 'bacon', 'sausage', 'ham', 'lamb', 'meat', 'venison', 'veal', 'steak', 'ribs', 'filet mignon', 'shrimp',
+    'snail', 'oyster', 'fish', 'tilapia', 'tuna', 'salmon', 'walleye', 'mussels', 'pepperoni', 'salami', 'patty', 'turkey']
+veggie_replacements = {
+    # 'tofu': ['beef', 'chicken', 'pork', 'shrimp', 'snail', 'oyster', 'mussels'],
+    'seitan': ['beef', 'chicken', 'pork', 'filet mignon', 'steak', 'venison', 'lamb', 'meat', 'ribs', 'turkey'],
+    'tempeh': ['fish', 'tilapia', 'tuna', 'salmon', 'walleye']}
 to_be_verbs = [
     'be', 'is', 'are', 'was'
     ]
@@ -70,26 +78,28 @@ tools = [
     'toaster', 'chopper', 'double boiler', 'steamer'
     ]
 
+
 class Step:
     def __init__(self, text):
         self.text = "You " + text.strip()[0:1].lower() + text.strip()[1:]
+        nlp = spacy.load('en_core_web_sm')
+        self.tokens = nlp(self.text)
         self.__parse__()
+        self.__get_time()
 
     def __parse__(self):
-        nlp = spacy.load('en_core_web_sm')
-        tokens = nlp(self.text)
         self.ingredients = []
-        for i in range(len(tokens)):
-            if (not tokens[i].tag_.startswith('NN')) or tokens[i].text in measurements_list:
+        for i in range(len(self.tokens)):
+            if (not self.tokens[i].tag_.startswith('NN')) or self.tokens[i].text in measurements_list:
                 continue
             for key in ingredients.keys():
                 ingredient = key.lower()
-                if tokens[i].text in ingredient:
-                    print("Starting with: " + tokens[i].text)
-                    potential_ingredient = tokens[i].text
+                if self.tokens[i].text in ingredient:
+                    # print("Starting with: " + self.tokens[i].text)
+                    potential_ingredient = self.tokens[i].text
                     j = i - 1
-                    while j > 0 and tokens[j].text in ingredient:
-                        potential_ingredient = tokens[j].text + " " + potential_ingredient
+                    while j > 0 and self.tokens[j].text in ingredient:
+                        potential_ingredient = self.tokens[j].text + " " + potential_ingredient
                         j -= 1
                     self.ingredients.append(potential_ingredient)
         removals = []
@@ -105,12 +115,44 @@ class Step:
         self.ingredients = [i for i in self.ingredients if i]
         self.verbs = []
         self.tools = []
-        for token in tokens:
+        for token in self.tokens:
             # print(token.text + "\t" + token.tag_ + "\t" + token.dep_)
-            if token.tag_ in ["VB", "VBP", "VBZ"] and token.text not in to_be_verbs:
+            if token.tag_ in ["VB", "VBP", "VBZ"] and token.text not in to_be_verbs and token.text in cooking_verbs:
                 self.verbs.append(token)
             elif token.text in tools:  # token.tag_ in ["NN"] and
                 self.tools.append(token.text)
+
+    def __get_time(self):
+        self.time = 0
+        self.primary_method = ''
+        last_verb = ''
+        for i in range(len(self.tokens)):
+            if self.tokens[i].text in cooking_verbs:
+                last_verb = self.tokens[i].text
+            if self.tokens[i].tag_ in ['CD', 'LS'] or self.tokens[i].text == 'an':
+                unit = self.tokens[i + 1].text
+                if unit == 'hours':
+                    try:
+                        t = int(Fraction(self.tokens[i].text)) * 60
+                        if t > self.time:
+                            self.time = t
+                            self.primary_method = last_verb
+                    except Exception as e:
+                        print(self.tokens[i].text)
+                if unit == 'hour':
+                    t = 60
+                    if t > self.time:
+                        self.time = t
+                        self.primary_method = last_verb
+                if unit == 'minutes':
+                    try:
+                        t = int(Fraction(self.tokens[i].text))
+                        if t > self.time:
+                            self.time = t
+                            self.primary_method = last_verb
+                    except Exception as e:
+                        print(self.tokens[i].text)
+
 
     def get_verbs(self):
         return [v.text for v in self.verbs]
@@ -214,6 +256,7 @@ def get_ingredients():
         name = name.strip()
         desc = desc.strip()
         prep = prep.strip()
+        # 0: quantity 1: unit 2: description 3: prep step 4: name
         ingredients[text] = (product if product > 0 else None, unit, desc, prep, name)
 
 
@@ -234,32 +277,61 @@ def get_instructions():
 
 def get_primary_method(steps):
     max = 0
-    max_step = 0
+    max_step = "0"
     for step in steps:
-        num = 0
-        tokens = step.text.split()
-        if 'minutes' in tokens:
-            num = tokens[tokens.index('minutes')-1]
-            if num > max:
-                max = num
-                max_step = step
-        if 'hour' in tokens:
-            num = 60
-            if num > max:
-                max = num
-                max_step = step
-        if 'hours' in tokens:
-            num = tokens[tokens.index('hours')-1]
-            if num > max:
-                max = num
-                max_step = step
+        if step.time > max:
+            max = step.time
+            max_step = step.primary_method
+        elif step.time == max:
+            if max_step in primary_verbs:
+                continue
+            max_step = step.primary_method
+    return "Primary cooking method is " + max_step + " for %d minutes." % max
+
+
+def convert_vegetarian(steps):
+    veg_ingredients = {}
+    for ingredient in ingredients:
+        veg_ingredients[ingredient] = ingredients[ingredient]
+        for meat in meat_products:
+            if meat in ingredients[ingredient][4]:
+                replacement = "tofu"
+                for veggie in veggie_replacements:
+                    if meat in veggie_replacements[veggie]:
+                        replacement = veggie
+                veg_ingredients[ingredient] = (
+                    ingredients[ingredient][0],
+                    ingredients[ingredient][1],
+                    ingredients[ingredient][2],
+                    ingredients[ingredient][3],
+                    replacement
+                )
+    display_recipe(veg_ingredients, steps)
+
+
+def display_recipe(ings, steps):
+    for external_rep, internal_rep in ings.items():
+        print(external_rep)
+        line = ""
+        for i in internal_rep:
+            line += ((str(i).strip() + " | ") if i else "_ | ")
+        print(">> " + line + "\n")
+    steps = get_instructions()
+    for x in range(len(steps)):
+        print("Step " + str(x + 1) + ": " + str(steps[x].text[4:5].upper()) + str(steps[x].text[5:]))
+        print(steps[x].get_verbs())
+        print(steps[x].get_tools())
+        print(steps[x].ingredients)
+        print(steps[x].primary_method + " %d minutes" % steps[x].time)
+        print("")
+    print(get_primary_method(steps))
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         get_recipe(sys.argv[1])
     else:
-        get_recipe(urls[-2])
+        get_recipe(urls[3])
     get_ingredients()
     for external_rep, internal_rep in ingredients.items():
         print(external_rep)
@@ -268,10 +340,17 @@ if __name__ == "__main__":
             line += ((str(i).strip() + " | ") if i else "_ | ")
         print(">> " + line + "\n")
     steps = get_instructions()
-    get_primary_method(steps)
     for x in range(len(steps)):
         print("Step " + str(x + 1) + ": " + str(steps[x].text[4:5].upper()) + str(steps[x].text[5:]))
         print(steps[x].get_verbs())
         print(steps[x].get_tools())
         print(steps[x].ingredients)
+        print(steps[x].primary_method + " %d minutes" % steps[x].time)
         print("")
+    print(get_primary_method(steps))
+    val = "0"
+    while val != "Q":
+        val = input("0: Original\n1: Vegetarian\nQ: Quit\n")
+        if val == "1":
+            convert_vegetarian(steps)
+    print("Goodbye!")
